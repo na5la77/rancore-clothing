80a4ba6084db9495db3beb5dd37721e66c737850
Shadi Nakhla <shadinakhla13@gmail.com>
2023-02-12 20:12:42 +0200

    migrated user context to redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
