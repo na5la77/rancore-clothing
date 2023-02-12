abbf196f44ee3e2a905f9b980cfa3de318762b6c
Shadi Nakhla <shadinakhla13@gmail.com>
2023-02-12 20:11:39 +0200

    migrated user context to redux

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import {compose, applyMiddleware} from 'redux'
+import logger from 'redux-logger'
+import { rootReducer } from './root-reducer'
+import { legacy_createStore as createStore } from "redux"
+
+
+//root reducer
+const middleWares = [logger]
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+
+export const store=createStore(rootReducer,undefined,composedEnhancers)
+
