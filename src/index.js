dac60536ab3aedf256e2c76af6b7ffe4516d5126
Shadi Nakhla <shadinakhla13@gmail.com>
2023-02-14 17:25:55 +0200

    migrated categories context to redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 import { Provider } from "react-redux";
 import "./index.scss";
@@ -15,11 +14,9 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
             <CartProvider>
               <App />
             </CartProvider>
-          </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
